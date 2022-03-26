16309f230faddd27a80504541633532b24ed3f04
Kenjura <github@kenjura.com>
2022-03-25 23:34:05 -0700

    fixed grid column styles

--- src/components/CharacterSheet.jsx
+++ src/components/CharacterSheet.jsx
@@ -22,58 +22,76 @@ export default function CharacterSheet() {
     }
 
     return <div class="character-sheet">
-        <section class="grid-left">
-            <header>Basics</header>
+        <div class="grid-left">
+            <section>
+                <header>Basics</header>
 
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <td>Beryl</td>
-                </tr>
-
-                <tr>
-                    <th>Level</th>
-                    <td>10</td>
-                </tr>
-
-                <tr>
-                    <th>Classes</th>
-                    <td>Psion 10</td>
-                </tr>
-            </table>
-
-        </section>
-        <section class="grid-left">
-            <header>Classes</header>
-            <table>
-                <tr>
-                    <th>Level</th>
-                    <th>Class</th>
-                </tr>
-                <tr>
-                    <td>1-10</td>
-                    <td>Psion</td>
-                </tr>
-            </table>
-        </section>
-        <section class="grid-left">
-            <header>Feats</header>
-            <ul>
-                <li>Dodge</li>
-                <li>Aim</li>
-            </ul>
-            <button onClick={onAddFeat}>add feat</button>
-            <Modal isShown={isAddFeatModalShown} onClose={onCloseAddFeatModal} title="Add Feat">
                 <table>
-                    <tbody>
-                        { feats.map(([,name,,desc]) => <tr key={name}>
-                            <th>{name}</th>
-                            <td>{desc}</td>
-                        </tr>)}
-                    </tbody>
+                    <tr>
+                        <th>Name</th>
+                        <td>Beryl</td>
+                    </tr>
+
+                    <tr>
+                        <th>Level</th>
+                        <td>10</td>
+                    </tr>
+
+                    <tr>
+                        <th>Classes</th>
+                        <td>Psion 10</td>
+                    </tr>
                 </table>
-            </Modal>
-        </section>
+
+            </section>
+            <section>
+                <header>Classes</header>
+                <table>
+                    <tr>
+                        <th>Level</th>
+                        <th>Class</th>
+                    </tr>
+                    <tr>
+                        <td>1-10</td>
+                        <td>Psion</td>
+                    </tr>
+                </table>
+            </section>
+            <section>
+                <header>Feats</header>
+                <ul>
+                    <li>Dodge</li>
+                    <li>Aim</li>
+                </ul>
+                <div>
+                    <button onClick={onAddFeat}>add feat</button>
+                </div>
+                <Modal isShown={isAddFeatModalShown} onClose={onCloseAddFeatModal} title="Add Feat">
+                    <table>
+                        <tbody>
+                            { feats.map(([,name,,desc]) => <tr key={name}>
+                                <th>{name}</th>
+                                <td>{desc}</td>
+                            </tr>)}
+                        </tbody>
+                    </table>
+                </Modal>
+            </section>
+        </div>
+        <div class="grid-right">
+            <section>
+                <header>Ability Scores</header>
+                <p>TBD</p>
+            </section>
+            <section>
+                <header>Attributes</header>
+                <p>TBD</p>
+            </section>
+            <section>
+                <header>Skills</header>
+                <p>TBD</p>
+            </section>
+        </div>
 
 
     </div>
